44070f9d08ba1972f1e238edbf60832ed6f9162f
Alejo Tabraj <alejandro.tabraj@culqi.com>
2024-08-21 08:30:24 -0300

    Remove homepage test

--- src/__tests__/app/Homepage.tsx
+++ src/__tests__/app/Homepage.tsx
@@ -2,9 +2,4 @@ import { render, screen } from '@testing-library/react'
 import HomePage from '@/app/page'
 
 describe('HomePage', () => {
-    it('Renders home page', () => {
-        render(<HomePage/>)
-        expect(screen.getByText('Templates')).toBeInTheDocument()
-    })
-
 })
